543adefe4a0d9bc37cfdb7d952229261e80e8dc7
KristofferEide06 <kristoffere.06@gmail.com>
2026-01-29 17:40:12 +0100

    Updated init_pinn_params func

--- src/project/model.py
+++ src/project/model.py
@@ -46,7 +46,17 @@ def init_pinn_params(cfg: Config, seed: int | None = None):
 
     # Placeholder initialization — replace this with your implementation
     pinn_params = {}
-
+    
+    nn_params = init_nn_params(cfg, key)
+    
+    alpha, k, h, P = jnp.array([0.25]), jnp.array([0.04]), jnp.array([2]), jnp.array([30]) #Reasonable if high insulation, check divide by zero for h = 0 error!
+    
+    pinn_params['nn'] = nn_params
+    pinn_params['log_alpha'] = np.log(alpha)
+    pinn_params['log_k'] = np.log(k)
+    pinn_params['log_h'] = np.log(h)
+    pinn_params['log_power'] = np.log(P)
+    
     #######################################################################
     # Oppgave 5.1: Slutt
     #######################################################################
